5a9633165b508dd73f460bf89dab3029ca04a8b1
KingInktvis <rikdevries253@hotmail.nl>
2016-11-14 14:50:22 +0100

    De schedular header is toegevoegt.

--- arduino/schedular.h
+++ arduino/schedular.h
@@ -1,35 +1,74 @@
 /*
-Bron: http://maxembedded.com/2011/06/avr-timers-timer0/ , https://maxembedded.wordpress.com/2011/06/28/avr-timers-timer1/
-https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
+Deze header geeft de mogelijkheid om taken eens in de x seconden uit te voeren.
+Voor gebruik dient de initSchedular() te zijn aangeroepen.
+Om een taak in de schedular te zetten moet addTask() worden aangeroepen.
+Om de schedular verder te laten functioneren moet callTasks() constant worden aangeroepen.
 */
 #include <util/delay.h>
-#define led_TIME 20
+#define NUMBER_OF_TASKS 2
 uint8_t secondCount;
-uint8_t ledTime;
+void (*task[NUMBER_OF_TASKS]) (void);
+uint8_t taskStart[NUMBER_OF_TASKS];
+uint8_t taskDelay[NUMBER_OF_TASKS];
+uint8_t taskindex;
+
+/*
+addTask() voegt een taak toe aan de schedular.
+Hiervoor wordt om 3 parameters gevraagt.
+Dit zijn de functie die moet worden aangroepen,
+Het moment dat deze taak voor het eerst moet worden uitgevoerd
+en hoeveel tijd er tussen het uitvoeren van de taak zit
+NOTE begin en delay moeten gelijk of groter dan 0 zijn en kleiner dan 60.
+*/
+void addTask(void(*function)(), uint8_t begin, uint8_t delay)
+{
+  task[taskindex] = function;
+  taskStart[taskindex] = begin;
+  taskDelay[taskindex] = delay;
+  taskindex++;
+}
+/*
+initSchedular() Initialiseerd de globale variabelen die vereist zijn.
+*/
 void initSchedular(void)
 {
-    secondCount = 0;
-    uint8_t ledTime = led_TIME;
-    DDRB |= (1 << PB0);
-    PORTB |= (1 << PB0);
+  taskindex = 0;
+  secondCount = 0;
 }
-void led()
-{
-  PORTB ^= (1 << PD0);
-}
+/*
+second verhoogt de secondenteller (secondCount) elke seconde mits aangeroepen.
+Verder wordt secondCount verlaagt bij het overschrijden van correcte waarden (60 en hoeger)
+*/
 void second(void)
 {
-  _delay_ms(900);
+  _delay_ms(999);
   secondCount++;
   if (secondCount >= 60) {
     secondCount = 0;
   }
 }
+/*
+Berekend wat het volgende tijstip is waarop een taak weer al moeten worden aangeroepn.
+*/
+uint8_t calcTime(uint8_t delay)
+{
+  uint8_t new;
+  new = secondCount + 60 - (60 - delay);
+  if (new >= 60) {
+    new = 0;
+  }
+  return new;
+}
+/*
+callTasks(void) kijkt over er taken zijn die moeten worden uitgevoerd.
+*/
 void callTasks(void)
 {
-  if (secondCount == ledTime) {
-    ledTime = ledTime + 60 - (60 - led_TIME);
-    led();
+  for (uint8_t i = 0; i < taskindex; i++) {
+    if (taskStart[i] == secondCount) {
+      (*task[i]) ();
+      taskStart[i] = calcTime(taskDelay[i]);
+    }
   }
   second();
 }
